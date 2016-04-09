32f4a310f43020d4981793d9942c68570b2bc405
Atanu-Sarkar <atanu.noida@gmail.com>
2016-04-09 18:39:02 +0530

    Correct usage of data types
    Java Session 1 Assignment3

---
+++ CurrectDataType.java
@@ -0,0 +1,18 @@
+package com.acadgild.session.one;
+
+public class CurrectDataType {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x;
+		int y;
+		long p;
+		String s;
+		
+		//x=p; long can't assign to int.
+		//y=s; String can't assign to int.
+
+	}
+
+}
